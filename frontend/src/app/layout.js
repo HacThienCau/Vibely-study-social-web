7e53d64637473c9fe70501bb1e0e6ae41d827696
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2025-02-21 14:48:24 +0700

    base_nextjs_and_install_packages

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Viebly",
+  description: "Mạng xã hội học tập kết nối các bạn học sinh, sinh viên",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
