3858f3b1e8b937d8e1dde3f8430cc3358ca2f4d5
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2025-02-27 14:39:35 +0700

    Google Oauth

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const connectDb = require('./config/db');
 require('dotenv').config();
 const authRoute = require('./routes/authRoute');
 const postRoute = require('./routes/postRoute');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./API/swagger.yaml');
 
 const app = express();
 app.use(express.json());
@@ -16,8 +20,10 @@ connectDb();
 //API Route
 app.use('/auth', authRoute);
 app.use('/users', postRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`📜 API Docs available at http://localhost:${PORT}/api-docs`);
     });
