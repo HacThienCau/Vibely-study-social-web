89025006c8c9fcc24a0d8897608326f1b0f15be7
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2025-03-14 23:06:48 +0700

    dontdo

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const Quotation = require('./model/Quotation');
 const userRoute = require('./routes/userRoute');
 const passport = require('./controllers/googleController');
 const scheduleRoute = require('./routes/scheduleRoute');
+const chatbotRoute = require('./routes/chatbotRoute');
 
 const YAML = require('yamljs');
 
@@ -44,6 +45,8 @@ app.use('/conversation', conversationRoute);
 app.use('/message', messageRoute);
 app.use('/users', userRoute);
 app.use('/schedules', scheduleRoute);
+app.use('/chatbot', chatbotRoute);
+
 
 // ✅ API lấy danh ngôn ngẫu nhiên
 app.get('/quotations/random', async (req, res) => {
@@ -65,4 +68,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`📜 API Docs available at http://localhost:${PORT}/api-docs`);
-    });
+});
