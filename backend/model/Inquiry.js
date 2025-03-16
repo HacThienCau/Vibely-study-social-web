3fce8f2f9c7a64d21ca0f47bc37916e36aaf134b
Hoanggiaminh <hoanggiaminh27012004@gmail.com>
2025-03-16 11:07:58 +0700

    fix support

--- backend/model/Inquiry.js
+++ backend/model/Inquiry.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const InquirySchema = new mongoose.Schema(
   {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     message: { type: String, required: true, trim: true },
     status: { type: String, enum: ["pending", "resolved"], default: "pending" },
   },
