679c66e112cb12bf5973941a6d061b02a3a68f9b
HacThienCau <22521641@gm.uit.edu.vn>
2025-03-27 11:28:17 +0700

    temporary fix for AdminPost

--- backend/routes/adminPostRoute.js
+++ backend/routes/adminPostRoute.js
@@ -2,12 +2,12 @@ const express = require('express');
 const adminAuthMiddleware = require('../middleware/adminAuthMiddleware');
 const { getAllPosts, getPostByUserId, deletePost, deleteComment, deleteReply, getSinglePost} = require('../controllers/postController');
 const router = express.Router();
-
-router.get('/posts', adminAuthMiddleware, getAllPosts);
-router.get('/:postId', adminAuthMiddleware, getSinglePost);
-router.get('/user/:id', adminAuthMiddleware, getPostByUserId);
-router.delete('/deletePost/:postId', adminAuthMiddleware, deletePost);
-router.delete('/deleteComment/:postId/:commentId', adminAuthMiddleware, deleteComment);
-router.delete('/deleteReply/:postId/:commentId/:replyId', adminAuthMiddleware, deleteReply);
+// xóa tạm adminAuthMiddleware vì chưa có đăng nhập => token hết hạn => lỗi
+router.get('/posts',  getAllPosts);
+router.get('/:postId',  getSinglePost);
+router.get('/user/:id', getPostByUserId);
+router.delete('/deletePost/:postId',  deletePost);
+router.delete('/deleteComment/:postId/:commentId',  deleteComment);
+router.delete('/deleteReply/:postId/:commentId/:replyId',  deleteReply);
 
 module.exports = router;
