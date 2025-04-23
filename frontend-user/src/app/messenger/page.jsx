366ad5d9e819669b58e33f8008e2c32d9d961ca4
HacThienCau <22521641@gm.uit.edu.vn>
2025-04-23 08:47:35 +0700

    fix LeftSideBar appearance

--- frontend-user/src/app/messenger/page.jsx
+++ frontend-user/src/app/messenger/page.jsx
@@ -11,6 +11,7 @@ import Message from "../components/message/Message";
 import "./messenger.css";
 import { PiDotsThreeVerticalBold } from "react-icons/pi";
 import { IoSend } from "react-icons/io5";
+import LeftSideBar from "../components/LeftSideBar";
 
 
 const Messenger = () => {
@@ -305,6 +306,9 @@ const Messenger = () => {
     const [openChat, setOpenChat] = useState(window.matchMedia("(min-width: 768px)").matches)   //<md
     return (
         <div className="pt-14 messenger">
+            <div className="md:hidden">
+            <LeftSideBar/>
+            </div>
             {/* Sidebar danh sách hội thoại */}
             <div className="chatMenu">
                 <div className="chatMenuWrapper">
