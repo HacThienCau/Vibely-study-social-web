70d422e06f44522858cb36c89def07a7b6393b3a
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2025-04-30 23:26:28 +0700

    fix bugs

--- backend/routes/documentRoute.js
+++ backend/routes/documentRoute.js
@@ -3,39 +3,25 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 const {
     getFilteredDocuments,
-    createDocument,
     getDocumentById,
-    updateDocument,
-    deleteDocument,
     getLevels,
     getSubjectsByLevel,
-    createLevel,
-    createSubject,
     saveDocument
 } = require("../controllers/documentController");
 
-// Route lấy danh sách tài liệu theo level
-router.get("/levels", getLevels);
+// Route lấy danh sách cấp học
+router.get("/levels", authMiddleware, getLevels);
 
-// Route lấy danh sách môn học theo levelId
-router.get("/subjects/:levelId", getSubjectsByLevel);
-
-// Route tạo mới level và subject
-router.post("/levels", authMiddleware, createLevel);
-router.post("/subjects", authMiddleware, createSubject);
+// Route lấy danh sách môn học theo cấp học
+router.get("/subjects/:levelId", authMiddleware, getSubjectsByLevel);
 
 // Route lưu tài liệu
 router.post("/save", authMiddleware, saveDocument);
 
-// Route lấy danh sách tài liệu, tạo mới tài liệu, cập nhật tài liệu, xóa tài liệu
-router.route("/")
-    .get(authMiddleware, getFilteredDocuments)
-    .post(authMiddleware, createDocument);
+// Route lấy danh sách tài liệu theo điều kiện lọc
+router.get("/", authMiddleware, getFilteredDocuments);
 
-// Route lấy tài liệu theo ID, cập nhật tài liệu, xóa tài liệu
-router.route("/:id")
-    .get(authMiddleware, getDocumentById)
-    .put(authMiddleware, updateDocument)
-    .delete(authMiddleware, deleteDocument);
+// Route lấy tài liệu theo ID
+router.get("/:id", authMiddleware, getDocumentById);
 
 module.exports = router;
